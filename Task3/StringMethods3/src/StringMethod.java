d15cd90c1ff808d7a73c2d418938f2db4cece85e
sreechandanam <sreechandanamadamanchi@gmail.com>
2020-12-06 19:52:31 +0530

    modified print statement1

--- Task3/StringMethods3/src/StringMethod.java
+++ Task3/StringMethods3/src/StringMethod.java
@@ -28,7 +28,7 @@ public class StringMethod {
 		System.out.println("Check if string is empty 1: " + str.isEmpty());
 		System.out.println("Check if string is empty 2: " + str4.isEmpty());
 		System.out.println("Trim the leading and trailing spaces: " + str5.trim());
-		System.out.println("Replace Example: " + Chandu.replace('e', 'a'));
+		System.out.println("ReplaceExample: " + Chandu.replace('e', 'a'));
 		System.out.println("Substring Example 1: " + str.substring(5));
 		System.out.println("Substring Example 2: " + str.substring(5, 10));
 		
